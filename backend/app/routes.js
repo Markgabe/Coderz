572f7a2235ed49964ece8e7b2335b276859ca35f
Mark <marcos98cf@gmail.com>
2021-11-03 23:19:14 -0300

    Fix: Fixed auth on backend

--- backend/app/routes.js
+++ backend/app/routes.js
@@ -5,12 +5,13 @@ import loginController from './controllers/loginController';
 const router = express.Router();
 
 router.post('/login', loginController.login);
-router.post('/token', loginController.refreshToken);
+router.post('/token', loginController.refreshTokenFunction);
 router.delete('/logout', loginController.logout);
+router.post('/user', userController.createUser);
 
+// All routes bellow will pass through jwt authorization
 router.use(loginController.authenticateToken);
 
-router.post('/user', userController.createUser);
 router.get('/users', userController.getAll);
 
 module.exports = router;
