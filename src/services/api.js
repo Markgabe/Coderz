2f4f5aff0b560c8e5a9e1999ba8b9863b80d4853
Mark <marcos98cf@gmail.com>
2021-11-12 00:38:39 -0300

    Feat: Added authorization token by default on all requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+axios.defaults.headers.common.Authorization = `Bearer: ${localStorage.getItem('accessToken')}`;
+
 export default axios.create({
   baseURL: process.env.REACT_APP_SERVER_PATH,
   timeout: 4000,
