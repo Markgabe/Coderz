acef55816ed7ee01d127e0fd203194bdec862ac4
Mark <marcos98cf@gmail.com>
2021-11-18 01:15:41 -0300

    Feat: Added submissionEvaluator service class

---
+++ src/services/submissionEvaluator.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-new-func */
+export function evaluateSubmission(code, testCases) {
+  if (code === '') return false;
+
+  const functionDefinition = extractFunctionFromCode(code);
+  const userFunction = new Function(wrapFunctionBody(functionDefinition));
+
+  return testCases.every((testCase) => evaluateTestCase(testCase, userFunction));
+}
+
+function extractFunctionFromCode(code) {
+  const regex = /^function\s\w+\(.*\)\s*\{(.|\n)*\}\n/gm;
+  return regex.exec(code)[0];
+}
+
+function wrapFunctionBody(functionBody) {
+  return `{ return ${functionBody}}`;
+}
+
+function evaluateTestCase(testCase, evaluatedFunction) {
+  const functionReturn = evaluatedFunction.call(null)
+    .call(null, ...Object.values(testCase.input));
+  return functionReturn.toString() === testCase.output.resultado.toString();
+}
