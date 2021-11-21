ca55cadb85024e65e97e8119e644893865026d05
Mark <marcos98cf@gmail.com>
2021-11-20 22:45:21 -0300

    Feat: Added loading screen if content not loaded

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react';
 import { Route, Redirect, Router } from 'react-router-dom';
+
+import Loader from 'react-loader-spinner';
+
 import history from './history';
 
 import { Context } from './contexts/AuthContext';
@@ -8,11 +11,23 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Challenge from './pages/Challenge';
+import ChallengeSelection from './pages/ChallengeSelection';
 
 function CustomRoute({ isPrivate, ...rest }) {
   const { loading, authenticated } = useContext(Context);
 
-  if (loading) return <></>;
+  if (loading) {
+    return (
+      <Loader
+        type='Oval'
+        color='#00BFFF'
+        height={100}
+        width={100}
+        timeout={3000}
+      />
+    );
+  }
+
   if (isPrivate && !authenticated) return <Redirect to='/login' />;
 
   return <Route {...rest} />;
@@ -24,6 +39,12 @@ export default function Routes() {
       <CustomRoute isPrivate exact path='/' component={Home} />
       <CustomRoute path='/login' component={Login} />
       <CustomRoute path='/register' component={Register} />
+      <CustomRoute
+        isPrivate
+        exact
+        path='/challenge'
+        component={ChallengeSelection}
+      />
       <CustomRoute isPrivate path='/challenge/:id' component={Challenge} />
     </Router>
   );
