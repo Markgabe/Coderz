5e3303c7cdd7533ed2d7bb05aaa43eadc475a057
Mark <marcos98cf@gmail.com>
2021-11-24 23:49:06 -0300

    Style: Recreated challenge selection screen

--- src/pages/ChallengeSelection/index.jsx
+++ src/pages/ChallengeSelection/index.jsx
@@ -1,36 +1,79 @@
+/* eslint-disable function-paren-newline */
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import { ChallengeCard } from './styles';
+import {
+  Container,
+  ChallengeArea,
+  ChallengeCard,
+  CategoryGroup,
+  CategoryCard,
+  CategoryLabel,
+  CategoryIcon,
+  VerticalSeparator,
+} from './styles';
+
 import BaseScreen from '../../components/BaseScreen';
-
-import history from '../../history';
 
 import api from '../../services/api';
 
+import Categories from '../../utils/Categories';
+import { groupBy } from '../../utils/ArrayUtils';
+
 export default function Challenge() {
-  const [challenges, setChallenges] = useState([]);
+  const [challengeMap, setChallengeMap] = useState([]);
+  const [selected, setSelected] = useState('math');
 
   useEffect(async () => {
     try {
       const { data } = await api.get('/challenges');
-      setChallenges(data);
+      setChallengeMap(groupBy(data, (d) => d.category));
     } catch (err) {
       // eslint-disable-next-line no-console
       console.log(err);
     }
   }, []);
+
+  const renderChallenge = (challenge) => (
+    <ChallengeCard key={challenge.id}>
+      <h2>{challenge.name}</h2>
+      <p>{challenge.description}</p>
+      <div>
+        <h4>+{challenge.gainedXP}XP</h4>
+        <Link to={`/challenge/${challenge.id}`}>Quero tentar!</Link>
+      </div>
+    </ChallengeCard>
+  );
+
+  const renderCategory = (category) => (
+    <CategoryCard
+      key={category.index}
+      color={category.color}
+      selected={selected === category.name}
+      onClick={() => setSelected(category.name)}
+    >
+      <CategoryLabel>{category.label}</CategoryLabel>
+      <CategoryIcon svg={category.icon} />
+    </CategoryCard>
+  );
+
   return (
     <BaseScreen activePage='challenge'>
-      {challenges.length > 0 &&
-        challenges.map((challenge) => (
-          <ChallengeCard key={challenge.id}>
-            <h2>{challenge.name}</h2>
-            <p>{challenge.description}</p>
-            <button onClick={() => history.push(`/challenge/${challenge.id}`)}>
-              Abrir
-            </button>
-          </ChallengeCard>
-        ))}
+      <Container>
+        <CategoryGroup>
+          {Categories.map((category) => renderCategory(category))}
+        </CategoryGroup>
+
+        <VerticalSeparator />
+
+        <ChallengeArea>
+          {Array.from(challengeMap).map(
+            ([category, challenges]) =>
+              category === selected &&
+              challenges.map((challenge) => renderChallenge(challenge))
+          )}
+        </ChallengeArea>
+      </Container>
     </BaseScreen>
   );
 }
--- src/pages/ChallengeSelection/styles.js
+++ src/pages/ChallengeSelection/styles.js
@@ -4,39 +4,118 @@ import { fadeIn } from 'react-animations';
 
 const fadeAnimation = keyframes`${fadeIn}`;
 
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`;
+
+export const ChallengeArea = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: calc(100% - 260px);
+  gap: 20px;
+  overflow-y: auto;
+  justify-content: center;
+`;
+
 export const ChallengeCard = styled.div`
   display: flex;
   flex-direction: column;
   margin: 10px 30px;
   padding: 20px;
 
-  @media (max-width: 1366px) {
-    min-width: 400px;
-    min-height: 200px;
-  }
-
-  @media (min-width: 1367px) {
-    min-width: 500px;
-    min-height: 300px;
-  }
+  height: 30%;
+  width: 40%;
 
   background-color: white;
   box-shadow: 2px 2px 8px 2px rgba(0, 0, 0, 0.25);
 
   animation: ${fadeAnimation} 0.5s;
+  transition: all 0.2s;
+
+  h2 {
+    color: #003b6d;
+  }
 
   p {
+    color: #676767;
     margin: 10px 0;
   }
+  
+  div {
+    display: flex;
+    margin-top: auto;
 
-  button {
-    background: #6699cc;
-    width: 200px;
-    border: none;
-    color: white;
-    font-weight: bold;
-    font-family: 'Montserrat', sans-serif;
-    margin: auto auto 0 0;
-    height: 30px;
+    h4 {
+      color: #3e8924;
+      margin: auto auto 0 0;
+    }
+    
+    a {
+      background: #6699cc;
+      cursor: pointer;
+      padding: 5px 20px;
+      border: none;
+      color: white;
+      font-weight: bold;
+      font-family: 'Montserrat', sans-serif;
+      margin: auto 0 0 auto;
+      height: 30px;
+      text-decoration: none;
+
+      &:hover {
+        filter: brightness(110%);
+      }
+    }
   }
+
+  &:hover {
+    transform: scale(1.025);
+  }
+`;
+
+export const VerticalSeparator = styled.div`
+  display: flex;
+  height: 100%;
+  width: 1px;
+  background-color: #C4C4C4;
+
+  margin-left: 20px;
+`;
+
+export const CategoryGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 20%;
+  min-width: 210px;
+  gap: 20px;
+`;
+
+export const CategoryCard = styled.div`
+  display: flex;
+  width: 100%;
+  height: 80px;
+  cursor: pointer;
+  background-color: ${(props) => props.color};
+  box-shadow: 2px 2px 8px 2px rgba(0, 0, 0, 0.25);
+
+  filter: ${(props) => (props.selected ? 'none' : 'opacity(60%)')};
+
+  &:hover {
+    filter: opacity(100%);
+  }
+`;
+
+export const CategoryLabel = styled.p`
+  color: white;
+  margin: auto auto auto 10px;
+  font-size: 1.6em;
+`;
+
+export const CategoryIcon = styled(({ svg, ...props }) => svg.render(props))`
+  color: white;
+  margin: auto 10px auto auto;
+  width: 40px;
+  height: 40px;
 `;
