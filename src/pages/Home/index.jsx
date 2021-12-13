759fbf204d97d68ef2c736c8621e147c9b21c9fd
mark <mgabriel@id.uff.br>
2021-12-13 17:54:46 -0300

    Feat: Added next challenge card to home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,12 +4,22 @@ import { CardArea } from './styles';
 
 import BaseScreen from '../../components/BaseScreen';
 import Card from '../../components/Card';
+import NextChallenge from '../../components/NextChallenge';
 
 export default function Home() {
   return (
     <BaseScreen activePage='home'>
       <CardArea>
-        <Card />
+        <Card>
+          <NextChallenge
+            challenge={{
+              id: 1,
+              name: 'Soma de dois números',
+              category: 'math',
+              level: 1,
+            }}
+          />
+        </Card>
         <Card />
         <Card />
         <Card />
